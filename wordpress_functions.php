868df2519c5311722b1bf6091165d860f4bc3876
jetfire21 <freerun-2012@yandex.ru>
2017-10-18 22:07:07 +0300

    small changes

--- wordpress_functions.php
+++ wordpress_functions.php
@@ -1272,6 +1272,22 @@ function as21_temp_func(){
 	}
 }
 
+/* **** as21 запретить переход всем пользователем к консоли /wp-admin кроме администраторов (limiting access to dashboard non-admin users) **** */
+add_action( 'init', 'blockusers_init' );
+function blockusers_init() {
+	if ( is_admin() && ! current_user_can( 'administrator' ) && ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+		wp_redirect( home_url() );
+		exit;
+	}
+}
+
+/* **** as21 hide adminbar for non-admin users **** */
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+	if (!current_user_can('administrator') && !is_admin()) {
+	  show_admin_bar(false);
+	}
+}
 
 /* **** as21 создание дополнительных метаполей у категории (в данном коде раздел) c плагином acf (advanced custom fields)
 Пример: каталоги четра->T-9.01 - раздел 1 кабина (список категорий) - раздел 2 Двигатель (список категорий) и т.д. **** */
