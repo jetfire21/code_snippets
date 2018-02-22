6347a341bd2484d1155cc81ce783ea7cd0932a7f
Alexey S <11636629+jetfire21@users.noreply.github.com>
2018-02-23 00:51:03 +0300

    Create js-plugins.js

---
+++ js-plugins.js
@@ -0,0 +1,5 @@
+#1------------
+	Masked Input plugin for jQuery
+	Copyright (c) 2007-2013 Josh Bush (digitalbush.com)
+	Licensed under the MIT license (http://digitalbush.com/projects/masked-input-plugin/#license)
+  валидация форм, провретка по маске,пример телефон (999) 999-99-99 postcode (999999) итд
