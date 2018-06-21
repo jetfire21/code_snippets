25de1bbc906026a3b744d9e8a1e29097ed245eb9
jetfire21 <freerun-2012@yandex.ru>
2018-06-21 11:49:41 +0400

    21-6-2018

--- wordpress_functions.php
+++ wordpress_functions.php
@@ -645,6 +645,23 @@ remove_action('wp_head', 'wp_shortlink_wp_head');
 //отключение xml-rpc
 add_filter('xmlrpc_enabled', '__return_false');
 
+
+// remove version from head
+remove_action('wp_head', 'wp_generator');
+
+// remove version from rss
+add_filter('the_generator', '__return_empty_string');
+
+// remove version from scripts and styles
+function shapeSpace_remove_version_scripts_styles($src) {
+	if (strpos($src, 'ver=')) {
+		$src = remove_query_arg('ver', $src);
+	}
+	return $src;
+}
+add_filter('style_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
+
 /* *** отключение REST API WP начиная с версии 4.4 *********** */
 
 // Отключаем сам REST API
